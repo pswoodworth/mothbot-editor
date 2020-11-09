c11b0f1ab855a170ddfa2e47b7cd87f7c40f2781
Parker Woodworth <pswoodworth@gmail.com>
2020-11-09 08:57:58 -0500

    initial

--- renderer.js
+++ renderer.js
@@ -4,3 +4,43 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+const { dialog } = require("electron").remote;
+const Avrgirl = require("avrgirl-arduino");
+
+const avrgirl = new Avrgirl({
+  board: "uno",
+});
+
+let pathToFlash;
+
+const chooseEl = document.querySelector("#choose-file");
+const flashEl = document.querySelector("#flash");
+
+chooseEl.addEventListener("click", function () {
+  console.log("finding file...");
+  const dialogPaths = dialog.showOpenDialogSync({
+    properties: ["openFile"],
+    filters: [{ name: "Binary Hex File", extensions: [".hex"] }],
+  });
+  pathToFlash = dialogPaths[0];
+  if (pathToFlash) {
+    console.log("PATH", flashEl);
+    flashEl.disabled = false;
+  }
+});
+
+flashEl.addEventListener("click", function () {
+  console.log(`flashing "${pathToFlash}"...`);
+  avrgirl.flash(pathToFlash, function (error) {
+    if (error) {
+      console.error(error);
+    } else {
+      console.info("done.");
+      dialog.showMessageBoxSync({
+        title: "Success!",
+        message: `Successfully flashed "${pathToFlash}"!`,
+      });
+    }
+  });
+});
